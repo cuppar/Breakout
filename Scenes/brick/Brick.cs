7f5ce246183aaa075c35c4f44f4359ca8064f1a5
cuppar <cuppar@163.com>
2024-06-21 15:05:19 +0800

    refactor folder name

---
+++ Scenes/brick/Brick.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+namespace Breakout.scenes.brick;
+
+public partial class Brick: StaticBody2D
+{
+    
+}
---
+++ Scenes/player_platform/PlayerPlatform.cs
@@ -0,0 +1,22 @@
+using Godot;
+
+namespace Breakout.scenes.player_platform;
+
+public partial class PlayerPlatform : CharacterBody2D
+{
+    private Vector2 _direction = Vector2.Zero;
+
+    [Export] public float Speed { get; set; } = 200;
+
+
+    public override void _Process(double delta)
+    {
+        _direction = Vector2.Zero;
+        if (Input.IsActionPressed("move_left")) _direction += Vector2.Left;
+
+        if (Input.IsActionPressed("move_right")) _direction += Vector2.Right;
+
+        Velocity = _direction * Speed;
+        MoveAndSlide();
+    }
+}
